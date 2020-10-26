c9a434a99dc8f5c722352981812f2db8dc6e8607
sean-sn <sean@supranational.net>
2020-10-26 09:45:59 -0400

    Added the rest of the functions to rust

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -29,7 +29,7 @@ extern "C" {
 
     pub fn bls12_g1multiexp(
         out: *mut byte,
-        input: *mut byte,
+        input: *const byte,
         in_len: usize,
     ) -> EIP2537_ERROR;
 
@@ -47,13 +47,13 @@ extern "C" {
 
     pub fn bls12_g2multiexp(
         out: *mut byte,
-        input: *mut byte,
+        input: *const byte,
         in_len: usize,
     ) -> EIP2537_ERROR;
 
     pub fn bls12_pairing(
         out: *mut byte,
-        input: *mut byte,
+        input: *const byte,
         in_len: usize,
     ) -> EIP2537_ERROR;
 
@@ -113,6 +113,112 @@ impl blstEIP2537Executor {
 
         Ok(output)
     }
+
+    pub fn g1_multiexp<'a>(input: &'a [u8]) -> Result<[u8; 128], &'static str> {
+        let mut output = [0u8; 128];
+
+        let err = unsafe {
+            bls12_g1multiexp(output.as_mut_ptr(), input.as_ptr(), input.len())
+        };
+
+        if err != EIP2537_SUCCESS {
+            return Err(blstEIP2537Executor::decode_eip2537_error(err));
+        }
+
+        Ok(output)
+    }
+
+    pub fn g2_add<'a>(input: &'a [u8]) -> Result<[u8; 256], &'static str> {
+        let mut output = [0u8; 256];
+
+        let err = unsafe {
+            bls12_g2add(output.as_mut_ptr(), input.as_ptr(), input.len())
+        };
+
+        if err != EIP2537_SUCCESS {
+            return Err(blstEIP2537Executor::decode_eip2537_error(err));
+        }
+
+        Ok(output)
+    }
+
+    pub fn g2_mul<'a>(input: &'a [u8]) -> Result<[u8; 256], &'static str> {
+        let mut output = [0u8; 256];
+
+        let err = unsafe {
+            bls12_g2mul(output.as_mut_ptr(), input.as_ptr(), input.len())
+        };
+
+        if err != EIP2537_SUCCESS {
+            return Err(blstEIP2537Executor::decode_eip2537_error(err));
+        }
+
+        Ok(output)
+    }
+
+    pub fn g2_multiexp<'a>(input: &'a [u8]) -> Result<[u8; 256], &'static str> {
+        let mut output = [0u8; 256];
+
+        let err = unsafe {
+            bls12_g2multiexp(output.as_mut_ptr(), input.as_ptr(), input.len())
+        };
+
+        if err != EIP2537_SUCCESS {
+            return Err(blstEIP2537Executor::decode_eip2537_error(err));
+        }
+
+        Ok(output)
+    }
+
+    pub fn pairing<'a>(input: &'a [u8]) -> Result<[u8; 32], &'static str> {
+        let mut output = [0u8; 32];
+
+        let err = unsafe {
+            bls12_pairing(output.as_mut_ptr(), input.as_ptr(), input.len())
+        };
+
+        if err != EIP2537_SUCCESS {
+            return Err(blstEIP2537Executor::decode_eip2537_error(err));
+        }
+
+        Ok(output)
+    }
+
+    pub fn map_fp_to_g1<'a>(
+        input: &'a [u8],
+    ) -> Result<[u8; 128], &'static str> {
+        let mut output = [0u8; 128];
+
+        let err = unsafe {
+            bls12_map_fp_to_g1(output.as_mut_ptr(), input.as_ptr(), input.len())
+        };
+
+        if err != EIP2537_SUCCESS {
+            return Err(blstEIP2537Executor::decode_eip2537_error(err));
+        }
+
+        Ok(output)
+    }
+
+    pub fn map_fp2_to_g2<'a>(
+        input: &'a [u8],
+    ) -> Result<[u8; 256], &'static str> {
+        let mut output = [0u8; 256];
+
+        let err = unsafe {
+            bls12_map_fp2_to_g2(
+                output.as_mut_ptr(),
+                input.as_ptr(),
+                input.len(),
+            )
+        };
+
+        if err != EIP2537_SUCCESS {
+            return Err(blstEIP2537Executor::decode_eip2537_error(err));
+        }
+
+        Ok(output)
+    }
 }
 
 #[cfg(test)]
@@ -132,11 +238,12 @@ mod tests {
             let r = r.unwrap();
             let input_str = r.get(0).unwrap();
             let input = hex::decode(input_str).unwrap();
-            let expected_output = if let Some(s) = r.get(1) {
-                hex::decode(s).unwrap()
-            } else {
-                vec![]
-            };
+            let mut expected_output = vec![];
+            if let Some(s) = r.get(1) {
+                if expect_success == true {
+                    expected_output = hex::decode(s).unwrap()
+                }
+            }
 
             let value = test_function(&input);
             //println!("Actual: {:x?}", value);
@@ -147,8 +254,8 @@ mod tests {
                     }
                 }
                 Err(..) => {
-                    println!("ERROR: {:?}", value);
                     if expect_success == true {
+                        println!("ERROR: {:?}", value);
                         return false;
                     }
                 }
@@ -178,16 +285,123 @@ mod tests {
         assert!(success);
     }
 
-    // TODO - need to fix run_on_test_inputs to handle expected err msg
-    /*
-        #[test]
-        fn test_g1_not_on_curve() {
-            let p = "../test_vectors/g1_not_on_curve.csv";
-            let f = |input: &[u8]| {
-                blstEIP2537Executor::g1_mul(input).map(|r| r.to_vec())
-            };
-            let success = run_on_test_inputs(p, false, f);
-            assert!(success);
-        }
-    */
+    #[test]
+    fn test_g1_not_on_curve() {
+        let p = "../test_vectors/g1_not_on_curve.csv";
+        let f = |input: &[u8]| {
+            blstEIP2537Executor::g1_mul(input).map(|r| r.to_vec())
+        };
+        let success = run_on_test_inputs(p, false, f);
+        assert!(success);
+    }
+
+    #[test]
+    fn test_g1multiexp() {
+        let p = "../test_vectors/g1_multiexp.csv";
+        let f = |input: &[u8]| {
+            blstEIP2537Executor::g1_multiexp(input).map(|r| r.to_vec())
+        };
+        let success = run_on_test_inputs(p, true, f);
+        assert!(success);
+    }
+
+    #[test]
+    fn test_g2add() {
+        let p = "../test_vectors/g2_add.csv";
+        let f = |input: &[u8]| {
+            blstEIP2537Executor::g2_add(input).map(|r| r.to_vec())
+        };
+        let success = run_on_test_inputs(p, true, f);
+        assert!(success);
+    }
+
+    #[test]
+    fn test_g2mul() {
+        let p = "../test_vectors/g2_mul.csv";
+        let f = |input: &[u8]| {
+            blstEIP2537Executor::g2_mul(input).map(|r| r.to_vec())
+        };
+        let success = run_on_test_inputs(p, true, f);
+        assert!(success);
+    }
+
+    #[test]
+    fn test_g2_not_on_curve() {
+        let p = "../test_vectors/g2_not_on_curve.csv";
+        let f = |input: &[u8]| {
+            blstEIP2537Executor::g2_mul(input).map(|r| r.to_vec())
+        };
+        let success = run_on_test_inputs(p, false, f);
+        assert!(success);
+    }
+
+    #[test]
+    fn test_g2multiexp() {
+        let p = "../test_vectors/g2_multiexp.csv";
+        let f = |input: &[u8]| {
+            blstEIP2537Executor::g2_multiexp(input).map(|r| r.to_vec())
+        };
+        let success = run_on_test_inputs(p, true, f);
+        assert!(success);
+    }
+
+    #[test]
+    fn test_pairing() {
+        let p = "../test_vectors/pairing.csv";
+        let f = |input: &[u8]| {
+            blstEIP2537Executor::pairing(input).map(|r| r.to_vec())
+        };
+        let success = run_on_test_inputs(p, true, f);
+        assert!(success);
+    }
+
+    #[test]
+    fn test_invalid_subgroup_pairing() {
+        let p = "../test_vectors/invalid_subgroup_for_pairing.csv";
+        let f = |input: &[u8]| {
+            blstEIP2537Executor::pairing(input).map(|r| r.to_vec())
+        };
+        let success = run_on_test_inputs(p, false, f);
+        assert!(success);
+    }
+
+    #[test]
+    fn test_map_fp_to_g1() {
+        let p = "../test_vectors/fp_to_g1.csv";
+        let f = |input: &[u8]| {
+            blstEIP2537Executor::map_fp_to_g1(input).map(|r| r.to_vec())
+        };
+        let success = run_on_test_inputs(p, true, f);
+        assert!(success);
+    }
+
+    #[test]
+    fn test_invalid_fp_encoding() {
+        let p = "../test_vectors/invalid_fp_encoding.csv";
+        let f = |input: &[u8]| {
+            blstEIP2537Executor::map_fp_to_g1(input).map(|r| r.to_vec())
+        };
+        let success = run_on_test_inputs(p, false, f);
+        assert!(success);
+    }
+
+    #[test]
+    fn test_map_fp2_to_g2() {
+        let p = "../test_vectors/fp2_to_g2.csv";
+        let f = |input: &[u8]| {
+            blstEIP2537Executor::map_fp2_to_g2(input).map(|r| r.to_vec())
+        };
+        let success = run_on_test_inputs(p, true, f);
+        assert!(success);
+    }
+
+    #[test]
+    fn test_invalid_fp2_encoding() {
+        let p = "../test_vectors/invalid_fp2_encoding.csv";
+        let f = |input: &[u8]| {
+            blstEIP2537Executor::map_fp2_to_g2(input).map(|r| r.to_vec())
+        };
+        let success = run_on_test_inputs(p, false, f);
+        assert!(success);
+    }
 }
