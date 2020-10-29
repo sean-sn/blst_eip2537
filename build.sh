1f6146aa73368d5c842eae5f51c3524ee7a8f58c
sean-sn <sean@supranational.net>
2020-10-29 10:53:00 -0400

    Add --no-check-certificate to wget on test vector downloads

--- build.sh
+++ build.sh
@@ -14,39 +14,39 @@ if [ ! -d test_vectors ]; then
   mkdir test_vectors
   cd test_vectors
 
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/fp2_to_g2.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/fp_to_g1.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g1_add.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g1_mul.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g1_multiexp.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g1_not_on_curve.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g2_add.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g2_mul.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g2_multiexp.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g2_not_on_curve.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/invalid_fp2_encoding.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/invalid_fp_encoding.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/invalid_subgroup_for_pairing.csv
-  wget https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/pairing.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/fp2_to_g2.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/fp_to_g1.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g1_add.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g1_mul.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g1_multiexp.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g1_not_on_curve.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g2_add.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g2_mul.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g2_multiexp.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/g2_not_on_curve.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/invalid_fp2_encoding.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/invalid_fp_encoding.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/invalid_subgroup_for_pairing.csv
+  wget --no-check-certificate https://raw.githubusercontent.com/MariusVanDerWijden/go-ethereum/bls_fuzzer/tests/fuzzers/bls12381/csv/pairing.csv
 
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsG1Add.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsG1Mul.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsG1MultiExp.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsG2Add.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsG2Mul.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsG2MultiExp.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsMapG1.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsMapG2.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsPairing.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsG1Add.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsG1Mul.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsG1MultiExp.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsG2Add.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsG2Mul.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsG2MultiExp.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsMapG1.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsMapG2.json
-  wget https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsPairing.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsG1Add.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsG1Mul.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsG1MultiExp.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsG2Add.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsG2Mul.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsG2MultiExp.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsMapG1.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsMapG2.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/blsPairing.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsG1Add.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsG1Mul.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsG1MultiExp.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsG2Add.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsG2Mul.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsG2MultiExp.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsMapG1.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsMapG2.json
+  wget --no-check-certificate https://raw.githubusercontent.com/ethereum/go-ethereum/master/core/vm/testdata/precompiles/fail-blsPairing.json
 
   cd ..
 fi
